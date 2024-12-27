f700895738aedc742eaf35779b9c62e3b548b299
Dominic Ngoetjana <dominicngoetjana@gmail.com>
2024-12-27 10:31:29 +0200

    Basic Setup

---
+++ src/main/java/com/jvsnr/memory_monitoring_tool/MemoryMonitoringToolApplication.java
@@ -0,0 +1,13 @@
+package com.jvsnr.memory_monitoring_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoryMonitoringToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoryMonitoringToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvsnr/memory_monitoring_tool/MemoryMonitoringToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvsnr.memory_monitoring_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoryMonitoringToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
